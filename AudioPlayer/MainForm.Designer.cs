a29dfee7b05a5c5af1b0a2998130bfd7de078212
Indunil Sanjeewa <bluetech586@gmail.com>
2021-09-26 21:42:28 +0530

    Committed changes.

--- AudioPlayer/MainForm.Designer.cs
+++ AudioPlayer/MainForm.Designer.cs
@@ -31,6 +31,15 @@ namespace AudioPlayer
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.pnlBottom = new AudioPlayer.GradientPanel();
+            this.lstMusicBoxView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lblMessage = new System.Windows.Forms.Label();
             this.btnStop = new System.Windows.Forms.PictureBox();
             this.btnResume = new System.Windows.Forms.PictureBox();
@@ -42,15 +51,6 @@ namespace AudioPlayer
             this.pnlRight = new AudioPlayer.GradientPanel();
             this.pnlLeft = new AudioPlayer.GradientPanel();
             this.lblWelcome = new System.Windows.Forms.Label();
-            this.lstMusicBoxView = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.pnlBottom.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnStop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnResume)).BeginInit();
@@ -74,11 +74,86 @@ namespace AudioPlayer
             this.pnlBottom.Controls.Add(this.btnPlay);
             this.pnlBottom.Controls.Add(this.btnSelectSong);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 344);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 300);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(1904, 697);
+            this.pnlBottom.Size = new System.Drawing.Size(1904, 741);
             this.pnlBottom.TabIndex = 1;
             this.pnlBottom.TopColor = System.Drawing.Color.PaleGreen;
+            // 
+            // lstMusicBoxView
+            // 
+            this.lstMusicBoxView.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lstMusicBoxView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.lstMusicBoxView.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lstMusicBoxView.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstMusicBoxView.FullRowSelect = true;
+            this.lstMusicBoxView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstMusicBoxView.HideSelection = false;
+            this.lstMusicBoxView.Location = new System.Drawing.Point(264, 89);
+            this.lstMusicBoxView.MultiSelect = false;
+            this.lstMusicBoxView.Name = "lstMusicBoxView";
+            this.lstMusicBoxView.Size = new System.Drawing.Size(1415, 326);
+            this.lstMusicBoxView.TabIndex = 9;
+            this.lstMusicBoxView.UseCompatibleStateImageBehavior = false;
+            this.lstMusicBoxView.View = System.Windows.Forms.View.Details;
+            this.lstMusicBoxView.Visible = false;
+            this.lstMusicBoxView.MouseLeave += new System.EventHandler(this.lstMusicBoxView_MouseLeave);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FilePath";
+            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader1.Width = 0;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Album";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 400;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "#";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Title";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 787;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Length";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Contributing Artist";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 300;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Composer";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader7.Width = 300;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Size";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader8.Width = 100;
             // 
             // lblMessage
             // 
@@ -184,7 +259,7 @@ namespace AudioPlayer
             this.pnlMain.Font = new System.Drawing.Font("Garamond", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pnlMain.Location = new System.Drawing.Point(0, 0);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(1904, 344);
+            this.pnlMain.Size = new System.Drawing.Size(1904, 300);
             this.pnlMain.TabIndex = 0;
             this.pnlMain.TopColor = System.Drawing.Color.PaleGreen;
             // 
@@ -193,9 +268,9 @@ namespace AudioPlayer
             this.ptbPiano.BackColor = System.Drawing.Color.Transparent;
             this.ptbPiano.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ptbPiano.BackgroundImage")));
             this.ptbPiano.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ptbPiano.Location = new System.Drawing.Point(909, 127);
+            this.ptbPiano.Location = new System.Drawing.Point(915, 96);
             this.ptbPiano.Name = "ptbPiano";
-            this.ptbPiano.Size = new System.Drawing.Size(215, 210);
+            this.ptbPiano.Size = new System.Drawing.Size(215, 180);
             this.ptbPiano.TabIndex = 5;
             this.ptbPiano.TabStop = false;
             this.ptbPiano.MouseHover += new System.EventHandler(this.ptbPiano_MouseHover);
@@ -207,22 +282,23 @@ namespace AudioPlayer
             this.pnlRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pnlRight.BottomColor = System.Drawing.Color.Empty;
             this.pnlRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pnlRight.Location = new System.Drawing.Point(1503, 0);
+            this.pnlRight.Location = new System.Drawing.Point(1578, 0);
             this.pnlRight.Name = "pnlRight";
-            this.pnlRight.Size = new System.Drawing.Size(401, 344);
+            this.pnlRight.Size = new System.Drawing.Size(326, 300);
             this.pnlRight.TabIndex = 4;
             this.pnlRight.TopColor = System.Drawing.Color.PaleGreen;
             // 
             // pnlLeft
             // 
             this.pnlLeft.Angle = 60F;
+            this.pnlLeft.BackColor = System.Drawing.Color.DarkGreen;
             this.pnlLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlLeft.BackgroundImage")));
             this.pnlLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pnlLeft.BottomColor = System.Drawing.Color.Empty;
             this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlLeft.Location = new System.Drawing.Point(0, 0);
             this.pnlLeft.Name = "pnlLeft";
-            this.pnlLeft.Size = new System.Drawing.Size(401, 344);
+            this.pnlLeft.Size = new System.Drawing.Size(326, 300);
             this.pnlLeft.TabIndex = 3;
             this.pnlLeft.TopColor = System.Drawing.Color.PaleGreen;
             // 
@@ -231,86 +307,11 @@ namespace AudioPlayer
             this.lblWelcome.BackColor = System.Drawing.Color.Transparent;
             this.lblWelcome.Font = new System.Drawing.Font("Garamond", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWelcome.ForeColor = System.Drawing.Color.Gold;
-            this.lblWelcome.Location = new System.Drawing.Point(515, 40);
+            this.lblWelcome.Location = new System.Drawing.Point(518, 9);
             this.lblWelcome.Name = "lblWelcome";
             this.lblWelcome.Size = new System.Drawing.Size(931, 109);
             this.lblWelcome.TabIndex = 2;
             this.lblWelcome.Text = "Welcome To Creative Digital Studio";
-            // 
-            // lstMusicBoxView
-            // 
-            this.lstMusicBoxView.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.lstMusicBoxView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4,
-            this.columnHeader5,
-            this.columnHeader6,
-            this.columnHeader7,
-            this.columnHeader8});
-            this.lstMusicBoxView.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lstMusicBoxView.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lstMusicBoxView.FullRowSelect = true;
-            this.lstMusicBoxView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.lstMusicBoxView.HideSelection = false;
-            this.lstMusicBoxView.Location = new System.Drawing.Point(264, 89);
-            this.lstMusicBoxView.MultiSelect = false;
-            this.lstMusicBoxView.Name = "lstMusicBoxView";
-            this.lstMusicBoxView.Size = new System.Drawing.Size(1415, 326);
-            this.lstMusicBoxView.TabIndex = 9;
-            this.lstMusicBoxView.UseCompatibleStateImageBehavior = false;
-            this.lstMusicBoxView.View = System.Windows.Forms.View.Details;
-            this.lstMusicBoxView.Visible = false;
-            this.lstMusicBoxView.MouseLeave += new System.EventHandler(this.lstMusicBoxView_MouseLeave);
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "FilePath";
-            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader1.Width = 0;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Album";
-            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader2.Width = 400;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "#";
-            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader3.Width = 80;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Title";
-            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader4.Width = 787;
-            // 
-            // columnHeader5
-            // 
-            this.columnHeader5.Text = "Length";
-            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader5.Width = 100;
-            // 
-            // columnHeader6
-            // 
-            this.columnHeader6.Text = "Contributing Artist";
-            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader6.Width = 300;
-            // 
-            // columnHeader7
-            // 
-            this.columnHeader7.Text = "Composer";
-            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader7.Width = 300;
-            // 
-            // columnHeader8
-            // 
-            this.columnHeader8.Text = "Size";
-            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader8.Width = 100;
             // 
             // MainForm
             // 
